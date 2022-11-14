3a2c36da0a66b14cfe86c512140d871e83e7574f
rupak <rupaksapkota2000@gmail.com>
2022-11-14 21:01:39 +0545

    :sparkles: comment to post feature added

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -3,3 +3,4 @@ export * as followControllers from "./follow.controllers";
 export * as postControllers from "./post.controllers";
 export * as newsFeedControllers from "./newsfeed.controllers";
 export * as notificationControllers from "./notification.controllers";
+export * as commentControllers from "./comment.controllers";
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -5,6 +5,7 @@ import followRoutes from "./follow.routes";
 import postRoutes from "./post.routes";
 import newsFeedRoutes from "./newsfeed.routes";
 import notificationRoutes from "./notification.routes";
+import commentRoutes from "./comment.routes";
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use("/v1", followRoutes);
 app.use("/v1", postRoutes);
 app.use("/v1", newsFeedRoutes);
 app.use("/v1", notificationRoutes);
+app.use("/v1", commentRoutes);
 
 export default app;
