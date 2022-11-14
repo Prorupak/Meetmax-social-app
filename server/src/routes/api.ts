20775936243cc745702958209ebddfa5728faa07
rupak <rupaksapkota2000@gmail.com>
2022-11-14 19:58:55 +0545

    :sparkles: newsfeed features added

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -3,6 +3,7 @@ import authRoutes from "./auth.routes";
 import userRoutes from "./user.routes";
 import followRoutes from "./follow.routes";
 import postRoutes from "./post.routes";
+import newsFeedRoutes from "./newsfeed.routes";
 
 const app = express();
 
@@ -10,5 +11,6 @@ app.use("/v1/auth", authRoutes);
 app.use("/v1", userRoutes);
 app.use("/v1", followRoutes);
 app.use("/v1", postRoutes);
+app.use("/v1", newsFeedRoutes);
 
 export default app;
