d4e40dff23395f8a6b023e4b91bece22caf0174e
rupak <rupaksapkota2000@gmail.com>
2022-11-16 18:30:57 +0545

    :bricks: created rtk apiSlice and added tagTypes

---
+++ frontend/src/app/api/apiSlice.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const meetmaxUrl = import.meta.env.VITE_MEETMAX_URL || "http://localhost:9005";
+const meetmaxApiVersion = import.meta.env.VITE_MEETMAX_API_VERSION || "v1";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: `${meetmaxUrl}/api/${meetmaxApiVersion}`,
+  credentials: "include",
+  prepareHeaders: (headers) => {
+    return headers;
+  },
+});
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: baseQuery,
+
+  tagTypes: ["User", "Auth", "Feed", "Notifications", "Followers", "SuggestedUser", "Profile"],
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  endpoints: (builder: any) => ({}),
+});
